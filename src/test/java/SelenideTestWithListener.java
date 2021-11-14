e1706490676e23c0d1a2e0a94ca9752aa06e4675
KEmma6 <emma.chuyako@gmail.com>
2021-11-14 20:53:36 +0300

    fix test

--- src/test/java/SelenideTestWithListener.java
+++ src/test/java/SelenideTestWithListener.java
@@ -1,5 +1,7 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Description;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -7,12 +9,13 @@ import static com.codeborne.selenide.Selenide.*;
 public class SelenideTestWithListener extends TestBase{
 
     private static final String ACCOUNT_URL = "https://github.com/KEmma6";
-    private static final String ISSUE_NAME = "https://github.com/KEmma6";
+    private static final String ISSUE_NAME = "using Listener";
 
 
     @Test
     @Description("Проверка наличия Issues")
     public void testIssue(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
         open (ACCOUNT_URL);
 
